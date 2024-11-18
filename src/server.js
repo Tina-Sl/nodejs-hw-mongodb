c0bc7dc37b9a51c4e93736a7a5af932c2b47c02e
Tina-Sl <slobodenukvm@gmail.com>
2024-11-18 23:52:32 +0100

    step 5,6

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { env } from './utils/env.js';
 import cors from 'cors';
 import pino from 'pino-http';
 import { pinoConfigs } from './config/pinoConfigs.js';
-import { ContactsCollection } from './db/models/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 export const setupServer = () => {
   const PORT = Number(env('PORT', '3000'));
@@ -12,28 +12,39 @@ export const setupServer = () => {
   app.use(pino(pinoConfigs));
 
   app.get('/contacts', async (req, res) => {
-    const contacts = await ContactsCollection.find();
-    res.send({ status: 200, data: contacts });
-    console.log('con', contacts);
+    const data = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: data,
+    });
   });
 
-  app.get('/contacts/:id', async (req, res) => {
-    const { id } = req.params;
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
 
-    const contact = await ContactsCollection.findById(id);
-
-    if (contact === null) {
-      return res
-        .status(404)
-        .send({ status: 404, message: 'Contact not found' });
+    if (!contact) {
+      res.status(404).json({ status: 404, message: 'Contact not found' });
+      return;
     }
-    res.send({ status: 200, data: contact });
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
   });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
+  });
+
+  app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send({ status: 500, message: 'Internal server error' });
   });
 
   app.use((error, req, res, next) => {
