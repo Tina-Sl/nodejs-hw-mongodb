08023773cb51ef7785581a2bfafd71b4c043ffd9
Tina-Sl <slobodenukvm@gmail.com>
2025-01-05 09:03:23 +0100

    hw-7

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ import cookieParser from 'cookie-parser';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 // import { UPLOAD_DIR } from './constans/index.js';
 
 export const setupServer = () => {
@@ -28,6 +30,7 @@ export const setupServer = () => {
 
   app.use(cookieParser());
   // app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
