2f86419271acad6fade792541b061473e79245a0
Tina-Sl <slobodenukvm@gmail.com>
2025-01-06 16:42:42 +0100

    correction router

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,15 +1,15 @@
 import createHttpError from 'http-errors';
 import swaggerUI from 'swagger-ui-express';
-import fs from 'node:fs';
-
-import { SWAGGER_PATH } from '../constants/index.js';
+import fs from 'fs';
+import path from 'path';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+    const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf-8'));
+    return [swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch {
     return (req, res, next) =>
-      next(createHttpError.InternalServerError("Can't load swagger docs"));
+      next(createHttpError(500, "Can't load swagger docs"));
   }
 };
